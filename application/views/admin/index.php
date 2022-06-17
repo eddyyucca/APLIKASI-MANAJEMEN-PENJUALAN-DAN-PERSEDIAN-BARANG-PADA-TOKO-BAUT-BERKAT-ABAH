7254e285a738a8adb5c3ce0377451885722b4c61
eddyyucca <eddyyucca@gmail.com>
2022-06-18 06:02:27 +0800

    18062022 v2
    Signed-off-by: eddyyucca <eddyyucca@gmail.com>

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,26 +9,7 @@
             <div class="card-body">
 
                 <!-- mulai -->
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Statistik Surat Masuk</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart-bar">
-                            <canvas id="myBarChart"></canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Statistik Surat Keluar</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart-bar">
-                            <canvas id="surat_keluar"></canvas>
-                        </div>
-                    </div>
-                </div>
+
                 <!-- Content Row -->
             </div>
         </div>
